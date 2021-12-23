be2c4655d628558a0b4a5473652a0606b0dd446b
Moehringen <haroldstuttgart@gmail.com>
2021-12-23 16:52:22 +0800

    handleTransfer

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,33 +1,34 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
+import {SubstrateEvent} from "@subql/types";
+import {Transfer, Account} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
-
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
-    //Record block number
-    record.field1 = block.block.header.number.toNumber();
-    await record.save();
+export async function handleTransfer(event: SubstrateEvent): Promise<void> {
+    // Get data from the event
+    // The balances.transfer event has the following payload \[from, to, value\]
+    // logger.info(JSON.stringify(event));
+    const from = event.event.data[0];
+    const to = event.event.data[1];
+    const amount = event.event.data[2];
+    // logger.info(from + '' +  to + '' + amount);
+    
+    // ensure that our account entities exist
+    const fromAccount = await Account.get(from.toString());
+    if (!fromAccount) {
+        await new Account(from.toString()).save();
+    }
+    
+    const toAccount = await Account.get(to.toString());
+    if (!toAccount) {
+        await new Account(to.toString()).save();
+    }
+    
+    // Create the new transfer entity
+    const transfer = new Transfer(
+        `${event.block.block.header.number.toNumber()}-${event.idx}`,
+    );
+    transfer.blockNumber = event.block.block.header.number.toBigInt();
+    transfer.fromId = from.toString();
+    transfer.toId = to.toString();
+    transfer.amount = (amount as Balance).toBigInt();
+    await transfer.save();
 }
-
-export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, balance]}} = event;
-    //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = account.toString();
-    //Big integer type Balance of a transfer event
-    record.field3 = (balance as Balance).toBigInt();
-    await record.save();
-}
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.field5 = true;
-    await record.save();
-}
-
-
