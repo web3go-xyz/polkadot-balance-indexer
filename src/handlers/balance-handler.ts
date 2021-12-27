d0f86c1eb08bf570724d671fb39b5aa10c0dfffa
Moehringen <haroldstuttgart@gmail.com>
2021-12-27 16:16:22 +0800

    handle transfer finish

--- src/handlers/balance-handler.ts
+++ src/handlers/balance-handler.ts
@@ -24,6 +24,16 @@ const getID = async() => {
   }
 }
 
+const _createNewAccount = async (address: string) => {
+  const account = new Account(address);
+  account.freeBalance = BigInt(0);
+  account.reserveBalance = BigInt(0);
+  account.totalBalance = BigInt(0);
+  account.aid = await getID();
+  await account.save();
+  return account;
+}
+
 export const handleTransfer = async (substrateEvent: SubstrateEvent) => {
   const { event, block } = substrateEvent;
   const { timestamp: createdAt, block: rawBlock } = block;
@@ -34,22 +44,35 @@ export const handleTransfer = async (substrateEvent: SubstrateEvent) => {
   logger.info(`New Transfer happened!: ${JSON.stringify(event)}`);
 
   //ensure that our account entities exist
-  const fromAccount = await Account.get(from);
+  let fromAccount = await Account.get(from);
   if (!fromAccount) {
-    await new Account(from).save();
+    fromAccount = await _createNewAccount(from);
   }  
-  const toAccount = await Account.get(to.toString());
+  let toAccount = await Account.get(to);
   if (!toAccount) {
-    await new Account(to).save();
+    toAccount = await _createNewAccount(to);
   }
     
   // Create the new transfer entity
   const transfer = new Transfer(
         `${blockNum}-${event.}`,
     );
-    transfer.blockNumber = blockNum.toBigInt();
-    transfer.fromId = from;
-    transfer.toId = to;
-    transfer.balanceChange = balanceChange;
-    await transfer.save();
+  transfer.blockNumber = blockNum.toBigInt();
+  transfer.fromId = from;
+  transfer.toId = to;
+  transfer.balanceChange = balanceChange;
+  transfer.aid = await getID();
+  // Set the balance for from, to account
+  fromAccount.freeBalance = fromAccount.freeBalance - balanceChange;
+  fromAccount.totalBalance = fromAccount.freeBalance + fromAccount.reserveBalance;
+  toAccount.freeBalance = toAccount.freeBalance + balanceChange;
+  toAccount.totalBalance = toAccount.freeBalance + toAccount.reserveBalance;
+  // rescord the snapshot in Transfer entity
+  transfer.to_freeBalance = toAccount.freeBalance;
+  transfer.to_reserveBalance = toAccount.reserveBalance;
+  transfer.to_totalBalance = toAccount.totalBalance;
+  transfer.from_freeBalance = fromAccount.freeBalance;
+  transfer.from_reserveBalance = fromAccount.reserveBalance;
+  transfer.from_totalBalance = fromAccount.totalBalance;
+  await transfer.save();
 };
