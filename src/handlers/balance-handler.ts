2e280ec141fa9dc0ada35816270344cd6baba389
bianyunjian <bianyunjian@dev.com>
2021-12-27 16:56:32 +0800

    update package

--- src/handlers/balance-handler.ts
+++ src/handlers/balance-handler.ts
@@ -2,22 +2,22 @@ import { SubstrateBlock, SubstrateEvent } from '@subql/types';
 import { Account } from '../types/models/Account';
 import { Transfer } from '../types/models/Transfer';
 import { IDGenerator } from '../types/models/IDGenerator';
-import {Balance} from "@polkadot/types/interfaces";
+import { Balance } from "@polkadot/types/interfaces";
 
 
 const generaterID = "GENERATOR"
 
-const getID = async() => {
-  let generator =  await IDGenerator.get(generaterID);
+const getID = async () => {
+  let generator = await IDGenerator.get(generaterID);
   if (generator == null) {
-    generator =  new IDGenerator(generaterID);
+    generator = new IDGenerator(generaterID);
     generator.aID = BigInt(0).valueOf();
     await generator.save();
     logger.info(`first aID is : ${generator.aID}`);
     return generator.aID
   }
-  else{
-    generator.aID =  generator.aID + BigInt(1).valueOf()
+  else {
+    generator.aID = generator.aID + BigInt(1).valueOf()
     await generator.save()
     logger.info(`new aID is : ${generator.aID}`);
     return generator.aID
@@ -38,7 +38,7 @@ export const handleTransfer = async (substrateEvent: SubstrateEvent) => {
   const { event, block } = substrateEvent;
   const { timestamp: createdAt, block: rawBlock } = block;
   const { number: blockNum } = rawBlock.header;
-  const [from,to,balanceChange] = event.data.toJSON() as [string, string, bigint];
+  const [from, to, balanceChange] = event.data.toJSON() as [string, string, bigint];
 
 
   logger.info(`New Transfer happened!: ${JSON.stringify(event)}`);
@@ -47,25 +47,25 @@ export const handleTransfer = async (substrateEvent: SubstrateEvent) => {
   let fromAccount = await Account.get(from);
   if (!fromAccount) {
     fromAccount = await _createNewAccount(from);
-  }  
+  }
   let toAccount = await Account.get(to);
   if (!toAccount) {
     toAccount = await _createNewAccount(to);
   }
-    
+
   // Create the new transfer entity
   const transfer = new Transfer(
-        `${blockNum}-${createdAt}-${from}-${to}`,
-    );
+    `${blockNum}-${createdAt}-${from}-${to}`,
+  );
   transfer.blockNumber = blockNum.toBigInt();
   transfer.fromId = from;
   transfer.toId = to;
-  transfer.balanceChange = balanceChange;
+  transfer.balanceChange = BigInt(balanceChange);
   transfer.aid = await getID();
   // Set the balance for from, to account
-  fromAccount.freeBalance = fromAccount.freeBalance - balanceChange;
+  fromAccount.freeBalance = fromAccount.freeBalance - BigInt(balanceChange);
   fromAccount.totalBalance = fromAccount.freeBalance + fromAccount.reserveBalance;
-  toAccount.freeBalance = toAccount.freeBalance + balanceChange;
+  toAccount.freeBalance = toAccount.freeBalance + BigInt(balanceChange);
   toAccount.totalBalance = toAccount.freeBalance + toAccount.reserveBalance;
   // rescord the snapshot in Transfer entity
   transfer.to_freeBalance = toAccount.freeBalance;
